fb0a7f898e183d28fea1b9d9bdaf64b539448c99
khaled shaheen <kshaheen2003@gmail.com>
2023-10-17 20:46:45 -0500

    finished adding folders, listing folders, add task

--- static/script.js
+++ static/script.js
@@ -45,6 +45,8 @@ document.addEventListener('DOMContentLoaded', function () {
                     data.folder_names.forEach(folderName => {
                         const listItem = document.createElement('li');
                         listItem.textContent = folderName;
+                        const removeButton = createRemoveButton(folderName); // Call createRemoveButton
+                        listItem.appendChild(removeButton); // Add the remove button to the list item              
                         folderList.appendChild(listItem);
                     });
                 } else {
@@ -77,40 +79,79 @@ document.addEventListener('DOMContentLoaded', function () {
             });
     });
 
+    // Remove Folder button click event
+    function createRemoveButton(folderName) {
+        const removeButton = document.createElement('button');
+        removeButton.textContent = 'Remove';
+        removeButton.addEventListener('click', function () {
+          if (confirm(`Are you sure you want to remove folder: ${folderName}?`)) {
+            fetch('/removeFolder', {
+              method: 'POST',
+              body: JSON.stringify({ folderName: folderName }),
+              headers: {
+                'Content-Type': 'application/json'
+              }
+            })
+              .then(response => response.json())
+              .then(data => {
+                if (data.success) {
+                  refreshFolderList(); // Call the refreshFolderList function
+                } else {
+                  console.error('Error: ' + data.error);
+                }
+              })
+              .catch(error => {
+                console.error('Error: ' + error);
+              });
+          }
+        });
+        return removeButton;
+      }
+
     // Initial folder list retrieval
     refreshFolderList();
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    const addTaskButton = document.getElementById('addTaskButton');
-    const messageDiv = document.getElementById('message2');
 
-    addTaskButton.addEventListener('click', function () {
-        const folderName = document.getElementById('folder_names').value;
-        const taskName = document.getElementById('task_name').value;
-        const taskStatus = document.getElementById('task_status').value;
+document.addEventListener("DOMContentLoaded", function () {
+    // Get a reference to the folder list and task list elements
+    const folderList = document.getElementById('folder-list');
+    const taskList = document.getElementById('task-list');
 
-        const requestData = {
-            folder_name: folderName,
-            task_name: taskName,
-            task_status: taskStatus
-        };
-
-        fetch('/addTaskToFolder', {
-            method: 'POST',
-            body: JSON.stringify(requestData),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-           
-        })
-        .then(response => response.json())
-        .then(data => {
-            messageDiv.textContent = data.message;
-            // You can also refresh the folder list or perform other actions here.
-        })
-        .catch(error => {
-            messageDiv.innerHTML = `Error: ${error}`;
-        });
+    // Add a click event listener to the folder list
+    folderList.addEventListener("click", function (event) {
+        const target = event.target;
+        if (target.tagName === 'LI') {
+            const folderName = target.textContent.replace('Remove', '');
+            console.log(folderName);
+            // Make an AJAX request to your Python route
+            fetch(`/getFolderTask?folder_name=${folderName}`)
+                .then(response => response.json())
+                .then(data => {
+                    if (data.error) {
+                        alert(data.error);
+                    } else {
+                        // Display tasks in the task list
+                        taskList.innerHTML = ''; // Clear previous tasks
+                        data.folders.forEach(task => {
+                            const taskItem = document.createElement('div');
+                            taskItem.innerHTML = `Title: ${task.name}<br>Status: ${task.status}`;
+                            if (task.imageFileName && task.imageFileData) {
+                                const downloadLink = document.createElement('a');
+                                downloadLink.href = `data:image/png;base64,${task.imageFileData}`;
+                                downloadLink.download = task.imageFileName;
+                                downloadLink.textContent = `${task.imageFileName}`;
+                                taskItem.appendChild(downloadLink);
+                            }
+                            taskList.appendChild(taskItem);
+                        });
+                    }
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        }
     });
 });
+
+
