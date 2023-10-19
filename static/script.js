664a767c054185c71c259befad8c9e114488c418
khaled shaheen <kshaheen2003@gmail.com>
2023-10-18 19:57:27 -0500

    cleaned js

--- static/script.js
+++ static/script.js
@@ -117,12 +117,23 @@ document.addEventListener("DOMContentLoaded", function () {
     // Get a reference to the folder list and task list elements
     const folderList = document.getElementById('folder-list');
     const taskList = document.getElementById('task-list');
+    var folderNameFromClick = "";
+    
+    // Function to create an edit button for a task
+    function createEditButton() {
+        const editButton = document.createElement('button');
+        editButton.textContent = 'Edit';
+        editButton.classList.add('edit-task-button');
+        editButton.addEventListener('click', handleTaskEditClick);
+        return editButton;
+    }
 
     // Add a click event listener to the folder list
     folderList.addEventListener("click", function (event) {
         const target = event.target;
         if (target.tagName === 'LI') {
             const folderName = target.textContent.replace('Remove', '');
+            folderNameFromClick=folderName
             console.log(folderName);
             // Make an AJAX request to your Python route
             fetch(`/getFolderTask?folder_name=${folderName}`)
@@ -143,6 +154,9 @@ document.addEventListener("DOMContentLoaded", function () {
                                 downloadLink.textContent = `${task.imageFileName}`;
                                 taskItem.appendChild(downloadLink);
                             }
+                            // Add an edit button for each task
+                            const editButton = createEditButton();
+                            taskItem.appendChild(editButton);
                             taskList.appendChild(taskItem);
                         });
                     }
@@ -152,89 +166,20 @@ document.addEventListener("DOMContentLoaded", function () {
                 });
         }
     });
-});
-
-// script.js
-
-document.addEventListener('DOMContentLoaded', function() {
-    // Assuming you have an "Edit Task" button in your HTML with class "edit-button"
-    const editTaskButtons = document.querySelectorAll('.edit-button');
-    const newTaskNameInput = document.getElementById('newTaskName');
-    const newTaskStatusInput = document.getElementById('newTaskStatus');
-    const taskListContainer = document.getElementById('task-list');
-    var folderName;
-
-    editTaskButtons.forEach(button => {
-        button.addEventListener('click', function() {
-            folderName = prompt('Enter the folder name:');
-            if (folderName) {
-                fetchTasksForFolder(folderName);
-            } else {
-                // User canceled the prompt.
-                alert('Task editing canceled.');
-            }
-        });
-    });
-
-    function fetchTasksForFolder(folderName) {
-        fetch(`/getFolderTask?folder_name=${folderName}`)
-            .then(response => response.json())
-            .then(data => {
-                displayTasks(data.folders);
-            })
-            .catch(error => {
-                console.error('Error fetching tasks:', error);
-                alert('An error occurred while fetching tasks. Please try again later.');
-            });
-    }
-
-    function displayTasks(tasks) {
-        // Clear existing tasks from the UI
-        taskListContainer.innerHTML = '';
-
-        // Display tasks in the UI
-        tasks.forEach(task => {
-            const taskItem = document.createElement('div');
-            taskItem.className = 'task-item';
-            taskItem.innerHTML = `
-                <span class="task-name">${task.name}</span>
-                <span class="task-status">${task.status}</span>
-                <button class="edit-task-button">Edit Task</button>
-            `;
-            taskListContainer.appendChild(taskItem);
-        });
-
-        // Enable edit buttons for newly displayed tasks
-        enableEditButtons();
-    }
-
-    function enableEditButtons() {
-        const editTaskButtons = document.querySelectorAll('.edit-task-button');
-        editTaskButtons.forEach(button => {
-            button.addEventListener('click', handleTaskEditClick);
-        });
-    }
-
+    
     function handleTaskEditClick(event) {
-        const taskNameElement = event.target.parentElement.querySelector('.task-name');
-        const taskStatusElement = event.target.parentElement.querySelector('.task-status');
-        const oldTaskName = taskNameElement.textContent;
-        const oldTaskStatus = taskStatusElement.textContent;
+        const taskItem = event.target.parentElement;
+        const taskItemText = taskItem.textContent;
+    
+        const titleMatch = /Title: (.*?)Status:/.exec(taskItemText);
+        const statusMatch = /Status: (.*)/.exec(taskItemText);
 
-        const newTaskName = prompt('Enter the new task name:', oldTaskName);
-        const newTaskStatus = prompt('Enter the new task status:', oldTaskStatus);
+        if (titleMatch && statusMatch) {
+            const oldTaskName = titleMatch[1].trim();
+            const oldTaskStatus = statusMatch[1].trim();
 
-        // Perform the editing logic with folderName, oldTaskName, newTaskName, and newTaskStatus
-        // Send a POST request to save the changes on the server
-        // ...
-
-        // For demonstration purposes, update the UI immediately (without server interaction)
-        if (newTaskName && newTaskStatus) {
-            taskNameElement.textContent = newTaskName;
-            taskStatusElement.textContent = newTaskStatus;
-            alert('Task edited successfully!');
-        } else {
-            alert('Task editing canceled or input invalid.');
+            const newTaskName = prompt('Enter the new task name:', oldTaskName);
+            const newTaskStatus = prompt('Enter the new task status:', oldTaskStatus);
         }
 
         fetch('/updateTask', {
@@ -243,7 +188,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                folderName: folderName,
+                folderName: folderNameFromClick,
                 oldTaskName: oldTaskName,
                 newTaskName: newTaskName,
                 newTaskStatus: newTaskStatus
@@ -262,8 +207,10 @@ document.addEventListener('DOMContentLoaded', function() {
             console.error('Error editing task:', error);
             alert('An error occurred while editing the task. Please try again later.');
         });
-        refreshFolderList();
+    
     }
+
 });
 
-  
+
+
